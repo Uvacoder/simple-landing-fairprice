51561c680031b7a50d618fabe905de98d2077f89
Abstract UI <getabstractui@gmail.com>
2021-01-01 15:37:29 +0530

    Compatible with Tailwind CSS 2.0

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# FairPrice
-
-Customizable SaaS landing page template made using Tailwind CSS.
-[https://fairprice.netlify.com/](https://fairprice.netlify.com/)
+# tailwind-starter-v2
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,24 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: {
-    mode: 'all',
-    content: [
-      './public/**/*.html',
-    ],
-  },
-  theme: {
-    fontFamily: {
-      'sans': 'Public Sans, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
-    },
-    extend: {
-      colors: {
-        cyan: {
-          '500': '#23C0F5',
-          '600': '#0BB7F1',
-          '700': '#0AA5D9',
-        },
-        gray: {
-          '100': '#F4F6F8',
-          '300': '#C4CDD5',
-          '700': '#6A7C92',
-          '900': '#454F5B',
-        }
-      },
-    },
-  },
-  variants: {},
-  plugins: [],
-}
+	purge: ["./public/**/*.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			"sans": "Public Sans, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+		},
+		extend: {
+			colors: {
+				gray: colors.blueGray,
+				cyan: colors.cyan,
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio")],
+	corePlugins: {
+		animation: false,
+	},
+};
